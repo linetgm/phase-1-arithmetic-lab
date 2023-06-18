68723c2962b31d23c799cc7ca18d04755650a123
Linet <muthiigathoni@gmail.com>
2023-06-19 00:41:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let multiply;
+const num1 = 1;
+const num2 = 62;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+const num3 = 24;
+const num4 = 5;
+mod = (num3%num4);
+
+let max = Math.max(5,10,15,20);
